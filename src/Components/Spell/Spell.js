246ce080b1ad66bc36604628ced926ea3c9c4e4a
Aleksandar Stojanovic <aca_fpi@hotmail.com>
2020-04-13 08:26:35 +0200

    spell https

--- src/Components/Spell/Spell.js
+++ src/Components/Spell/Spell.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Spell = ({spell, passive}) => {
 
-    const src = passive ? `http://ddragon.leagueoflegends.com/cdn/10.7.1/img/passive/${spell.image.full}` : `http://ddragon.leagueoflegends.com/cdn/10.7.1/img/spell/${spell.image.full}`;
+    const src = passive ? `https://ddragon.leagueoflegends.com/cdn/10.7.1/img/passive/${spell.image.full}` : `https://ddragon.leagueoflegends.com/cdn/10.7.1/img/spell/${spell.image.full}`;
 
     return (
         <div className="spell">
